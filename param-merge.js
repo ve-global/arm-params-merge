d52fc13e71b6124a6c6ee66a0b3b868b10856c1e
Andy Royle <ajroyle@gmail.com>
2016-07-26 11:03:07 +0100

    initial commit

---
+++ param-merge.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var util = require('util');
+var merge = require('lodash.merge');
+
+var args = process.argv.slice(2);
+
+util.log('reading parameter file');
+var filename = args[0];
+var fileParams = require(path.resolve(filename));
+var values = args.slice(1);
+
+var params = {};
+
+util.log('parsing override values');
+values.forEach(function(f){
+  var bits = f.split('=');
+  params[bits[0].trim()] =  { value: bits[1].trim() };
+});
+
+fileParams.parameters = merge(fileParams.parameters, params);
+
+util.log('writing output file');
+fs.writeFile(filename, JSON.stringify(fileParams, null, 2), (err) => {
+  if(err){
+    util.log(err);
+  }
+  util.log('done');
+});
